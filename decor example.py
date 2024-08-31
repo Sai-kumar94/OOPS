39d0adb87dae71d1b169bbf711861d3b21eac34a
Sai-kumar94 <92421562+Sai-kumar94@users.noreply.github.com>
2024-08-31 15:03:50 +0530

    Update decor example.py

--- decor example.py
+++ decor example.py
@@ -6,12 +6,35 @@ def new_decorator(original_func):
         original_func()
         print('some extra code ,before the original function')
     return wrap_func
+
 def func_needs_decorator():
     print("i want to be decorated")
 func_needs_decorator()
 # o/p=i want to be decorated
 decorated_func=new_decorator(func_needs_decorator)
 decorated_func()
+
+
+#o/p=
+# i want to be decorated
+# some extra code ,before the original function
+# i want to be decorated
+# some extra code ,before the original function
+
+
+
+
+#  USING DECOR
+def new_decorator(original_func):
+    def wrap_func():
+        print('some extra code ,before the original function')
+        original_func()
+        print('some extra code ,before the original function')
+    return wrap_func
+@new_decorator
+def func_needs_decorator():
+    print("i want to be decorated")
+func_needs_decorator()
 
 #o/p=
 # i want to be decorated
