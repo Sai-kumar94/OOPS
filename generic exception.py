9c651539faea6445cd928830699c83f3888b0d37
Sai-kumar94 <92421562+Sai-kumar94@users.noreply.github.com>
2024-10-01 21:49:00 +0530

    Create generic exception.py

---
+++ generic exception.py
@@ -0,0 +1,14 @@
+#aliasing
+print('main start')
+l=[11,22,33]
+try:
+    print('try started')
+    ip=int(input('enter index position'))
+    print(l[ip])
+    print(a)
+    print('try ended')
+except Exception as E:
+    print('E')
+print('main ended')
+
+# 8722288880
