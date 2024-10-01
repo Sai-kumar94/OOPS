0de30e1aae4a25a1f1f775b6bd84e760237fdb3a
Sai-kumar94 <92421562+Sai-kumar94@users.noreply.github.com>
2024-10-01 21:49:19 +0530

    Create else block.py

---
+++ else block.py
@@ -0,0 +1,15 @@
+#aliasing
+print('main start')
+l=[11,22,33]
+try:
+    print('try started')
+    n1=int(input())
+    n2=int(input())
+    res=n1/n2
+    print('try ended')
+except Exception as E:
+    print(E)
+else:
+    print('inside else block')
+    print(res)
+print('main ended')
