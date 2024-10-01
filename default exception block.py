f392325dcead89f2d8f2c5f867b56374a36e291a
Sai-kumar94 <92421562+Sai-kumar94@users.noreply.github.com>
2024-10-01 21:49:23 +0530

    Create default exception block.py

---
+++ default exception block.py
@@ -0,0 +1,12 @@
+#aliasing
+print('main start')
+l=[11,22,33]
+try:
+    print('try started')
+    ip=int(input('enter index position'))
+    print(l[ip])
+    print(a)
+    print('try ended')
+except:
+    print('error is handled')
+print('main ended')
